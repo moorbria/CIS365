1d64dd16752c58fa9fcbe32e424aa1b34f74851f
moorbria <moorbria@mail.gvsu.edu>
2018-04-16 15:10:11 -0400

    Adding updated nn

--- Projects/Project3/nn.py
+++ Projects/Project3/nn.py
@@ -7,12 +7,12 @@ from tflearn.layers.estimator import regression
 from statistics import median, mean
 from collections import Counter
 
-LR = 1
+LR = .5
 
 env = gym.make('RocketLander-v0')
 env.reset()
 goal_steps = 750
-score_requirement = 1600
+score_requirement = 900
 initial_games = 2500
 
 def initial_population():
@@ -37,36 +37,34 @@ def initial_population():
         # for each frame in 200
         for episode in range(goal_steps):
             #env.render()
-            a = float(random.randrange(-100, 100))/100
+            a = 0 #float(random.randrange(-100, 100))/100
             b = float(random.randrange(-100, 100))/100
             c = float(random.randrange(-100, 100))/100
             action = [a, b, c]
-            
-
-
-
 
             # do it!
             observation, reward, done, info = env.step(action)
-    
-
-            # normailize data
-            norm_observation = normalize(observation)
-            #print(norm_observation)
 
             # custom reward function
             reward = 0
 
             # if angle is low
             if abs(observation[2]) < .5: 
-                reward += 5
+                reward += 1
+            
+            if abs(observation[2]) < .1: 
+                reward += 2
+
+            if abs(observation[9]) < 1:
+                reward += 2
+
   
             # if you are in the middle of the map
-            reward += (1 - abs(observation[0]))*3 # x position
+            #reward += (1 - abs(observation[0]))*3 # x position
 
             # if you are near the bottom and your speed is low
-            if observation[1] < -1.2 and observation[8] > 2.5 and observation[8] < 3.8:
-              reward += 3 
+            #if observation[1] < -1.2 and observation[8] > 2.5 and observation[8] < 3.8:
+            #  reward += 3 
 
             
             #if observation[1] < -1.3 and abs(observation[8]
@@ -135,6 +133,102 @@ def initial_population():
     
     return training_data
 
+def generation_population():
+    # [OBS, MOVES]
+    training_data = []
+    # all scores:
+    scores = []
+    # just the scores that met our threshold:
+    accepted_scores = []
+    score_requirement = 60
+    prev_obs = []
+
+    model = neural_network_model(10)
+    model.load('runs/1.model')
+
+    # iterate through however many games we want:
+    for episode in range(initial_games):
+        if episode % 25 is 0 and episode != 0:
+          print(str(episode/initial_games*100) + "% Complete")
+
+        score = 0
+        # moves specifically from this environment:
+        game_memory = []
+        # previous observation that we saw
+        prev_observation = []
+        # for each frame in 200
+        for episode in range(goal_steps):
+            if len(prev_obs)==0:
+                a = float(random.randrange(-100, 100))/100
+                b = float(random.randrange(-100, 100))/100
+                c = float(random.randrange(-100, 100))/100
+                action = [a, b, c]
+            else:
+                #print(prev_obs)
+                #print(model.predict(prev_obs.reshape(-1,len(prev_obs),1)))
+                action = model.predict(prev_obs.reshape(-1,len(prev_obs), 1))
+                action = action.tolist()[0]
+                #print(action)
+
+            observation, reward, done, info = env.step(action)
+
+            prev_obs = observation
+            game_memory.append([observation, action])
+            # custom reward function
+            reward = 0
+
+            # if angle is low
+            if abs(observation[2]) < .3: 
+                reward += 3
+
+            # notice that the observation is returned FROM the action
+            # so we'll store the previous observation here, pairing
+            # the prev observation to the action we'll take.
+            if len(prev_observation) > 0 :
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            score+=reward
+            if done: break
+
+        # IF our score is higher than our threshold, we'd like to save
+        # every move we made
+        # NOTE the reinforcement methodology here. 
+        # all we're doing is reinforcing the score, we're not trying 
+        # to influence the machine in any way as to HOW that score is 
+        # reached.
+        print(score)
+        if score >= score_requirement:
+            print(score)
+            accepted_scores.append(score)
+            for data in game_memory:
+                # convert to one-hot (this is the output layer for our neural network)
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+                else:
+                    output = data[1]
+                    
+                # saving our training data
+                training_data.append([data[0], output])
+
+        # reset env to play again
+        env.reset()
+        # save overall scores
+
+
+
+    # just in case you wanted to reference later
+    training_data_save = np.array(training_data)
+    np.save('saved2.npy',training_data_save)
+    
+    # some stats here, to further illustrate the neural network magic!
+    print('Average accepted score:',mean(accepted_scores))
+    print('Median score for accepted scores:',median(accepted_scores))
+    print("Count Accepted", len(accepted_scores))
+    
+    return training_data
+
 def neural_network_model(input_size):
 
     network = input_data(shape=[None, input_size, 1], name='input')
@@ -176,30 +270,24 @@ def train_model(training_data, model=False):
     model.fit({'input': X}, {'targets': y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openai_learning')
     return model
 
-
-def normalize(observation):
-  observation[0] = (observation[0] - (-3.5))/(3.5 - (-3.5))
-  observation[1] = (observation[1] - (-1.5))/(1.7 - (-1.5))
-  observation[2] = (observation[2] - (-12))/(12 - (-12))
-# observation[3] = (observation[3] - (-3.5))/(3.5 - (-3.5))
-# observation[4] = (observation[4] - (-3.5))/(3.5 - (-3.5))
-  observation[5] = (observation[5] - (-1.3))/(1 - (-1.3))
-  observation[6] = (observation[6] - (-1))/(1 - (-1))
-  observation[7] = (observation[7] - (-14))/(14 - (-14))
-  observation[8] = (observation[8] - (-2.7))/(0 - (-2.7))
-  observation[9] = (observation[9] - (-36))/(36 - (-36))
-
-  return observation
-
-
-#training_data = initial_population()
-training_data = np.load('saved.npy')
+# For Initial Runs
+training_data = initial_population()
 model = train_model(training_data)
+
+# New Model with previous training data
+#training_data = np.load('saved.npy')
+#model = train_model(training_data)
+
+# Trained Data New Model
+#training_data = generation_population()
+#training_data = np.load('saved2.npy')
+#model = train_model(training_data)
+
+# Rerun with saved model
 #model = neural_network_model(10)
-#model.load('1.model')
+#model.load('runs/1.model')
 
 scores = []
-choices = []
 x_position = []
 y_position = []
 angle = []
@@ -212,8 +300,6 @@ y_vel = []
 angle_vel = []
 
 for each_game in range(10):
-    
-
 
     score = 0
     game_memory = []
@@ -232,21 +318,10 @@ for each_game in range(10):
             #print(model.predict(prev_obs.reshape(-1,len(prev_obs),1)))
             action = model.predict(prev_obs.reshape(-1,len(prev_obs), 1))
             action = action.tolist()[0]
-            print(action)
+            #print(action)
 
-        choices.append(action)
                 
         new_observation, reward, done, info = env.step(action)
-        x_position.append(new_observation[0])
-        y_position.append(new_observation[1])
-        angle.append(new_observation[2])
-        first_leg.append(new_observation[3])
-        second_leg.append(new_observation[4])
-        throttle.append(new_observation[5])
-        gimbal.append(new_observation[6])
-        x_vel.append(new_observation[7])
-        y_vel.append(new_observation[8])
-        angle_vel.append(new_observation[9])
 
         prev_obs = new_observation
         game_memory.append([new_observation, action])
@@ -255,23 +330,9 @@ for each_game in range(10):
 
     scores.append(score)
 
-print("x_position min" + str(min(x_position)) + " | max: " + str(max(x_position)))
-print("y_position min" + str(min(y_position)) + " | max: " + str(max(y_position)))
-print("angle min" + str(min(angle)) + " | max: " + str(max(angle)))
-print("first_leg min" + str(min(first_leg)) + " | max: " + str(max(first_leg)))
-print("second_leg min" + str(min(second_leg)) + " | max: " + str(max(second_leg)))
-print("throttle min" + str(min(throttle)) + " | max: " + str(max(throttle)))
-print("gimbal min" + str(min(gimbal)) + " | max: " + str(max(gimbal)))
-print("x_vel min" + str(min(x_vel)) + " | max: " + str(max(x_vel)))
-print("y_vel min" + str(min(y_vel)) + " | max: " + str(max(y_vel)))
-print("angle_vel min" + str(min(angle_vel)) + " | max: " + str(max(angle_vel)))
 
 print('Average Score:',sum(scores)/len(scores))
 print('Max Score:', max(scores))
 
-model.save('1.model')
-
-
-
-
+model.save('2.model')
 
